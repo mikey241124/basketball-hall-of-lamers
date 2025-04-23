9c5147ed118f0f0fdcd8f4954df81a70c630d3f4
Yasser Ogando <70270618+Scrappy440@users.noreply.github.com>
2025-04-23 02:00:18 +0000

    fixed player generator

--- src/playerGenerator.cpp
+++ src/playerGenerator.cpp
@@ -6,7 +6,6 @@
 
 using namespace std;
 
-
 class playerGenerator{
 private:
     vector<string> allNames;
@@ -25,26 +24,27 @@ public:
             myFile.close();
         }
     }
-
+    //Int to float changes
+    //https://stackoverflow.com/questions/42138421/why-the-static-cast-on-time0-on-this-code
     playerList generateRandom(playerList inputtedList, int numberToGenerate){
-        srand(time(0));
+        srand(static_cast<unsigned>(time(0)));
 
         for(int i = 0; i < numberToGenerate; i++){
             string name;
             float points, assists, rebounds;
 
-            int randomNumber = (rand() % allNames.size()) + 1;
+            int randomNumber = rand() % allNames.size();
             name = allNames[randomNumber];
             allNames.erase(allNames.begin() + randomNumber);
 
-            float randomNum = (rand() % 45 + 1);
-            points = randomNum;
+            //float randomNum = (rand() % 45 + 1);
+            //randomNum = (rand() % 18 + 1);
+            //randomNum = (rand() % 25 + 1);
+            //https://www.geeksforgeeks.org/rand-and-srand-in-ccpp/
 
-            randomNum = (rand() % 18 + 1);
-            assists = randomNum;
-
-            randomNum = (rand() % 25 + 1);
-            rebounds = randomNum;
+            points = 1.0f + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (45.0f - 1.0f)));
+            assists = 1.0f + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (18.0f - 1.0f)));
+            rebounds = 1.0f + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (25.0f - 1.0f)));
 
             inputtedList.addPlayer(name, points, assists, rebounds);
         }
